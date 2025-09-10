73658aa373512c017cd7a17ca944c183fafb9f1b
realjay2 <ajtheprince2020@gmail.com>
2025-09-10 16:55:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const { useState, useEffect, useRef, useCallback } = React;
 const webhookURL = process.env.WEBHOOK_URL;
-const discordWebhookURL = 'https://discord.com/api/webhooks/1415162908031189002/5Jh3j-gknvofG8iXc6F5dXtkNrAgK68IKO0Qlgk-qPOj_C69L08M2L8itZtdK6m_ka9J';
+const discordWebhookURL = ${WEBHOOK_URL};
 
 // Function to get visitor IP and send to Discord webhook
 function getDeviceInfo() {
